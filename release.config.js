ef6f7433fe815a0bc82c4ee501d8b096e2f9b7ba
Radoslaw Cisz <radoslaw.cisz@gmail.com>
2022-08-04 11:34:27 +0200

    style: release.js

--- release.config.js
+++ release.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    branches: ["main"],
-    repositoryUrl: "https://github.com/radoscis/gh-reactapp.git",
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator',
-              '@semantic-release/github']
-}
+  branches: ["main"],
+  repositoryUrl: "https://github.com/radoscis/gh-reactapp.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
