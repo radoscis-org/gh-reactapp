ebc6e0e4f600a08e1988581daa5b1acfd472359a
Radoslaw Cisz <radoslaw.cisz@gmail.com>
2022-08-04 11:31:57 +0200

    feat: add sem versioning step

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["master"],
+    branches: ["main"],
     repositoryUrl: "https://github.com/radoscis/gh-reactapp.git",
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator',
               '@semantic-release/github']
